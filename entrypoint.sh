73b45ae8cc9aa39a8557620eb6f9439d88f34be2
Nicholas Dille <nicholasdille@users.noreply.github.com>
2020-03-24 08:51:14 +0100

    Simplified

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,7 @@
 set -o errexit
 
 deluser user
-#if ! grep -q ":x:${gid}:" /etc/group; then
-    addgroup -g ${gid} user
-#fi
-#if ! grep -q ":x:${uid}:" /etc/passwd; then
-    adduser -u ${uid} -D -G user user
-#fi
-echo "source /wrapper.sh" >/home/user/.bashrc
-chown user:user /home/user/.bashrc
+addgroup -g ${gid} user
+adduser -u ${uid} -D -G user user
 
 exec gosu ${uid}:${gid} rootlesskit buildkitd "$@"
